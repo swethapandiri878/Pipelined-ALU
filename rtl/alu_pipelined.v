0f71f961ca3fbd1ec909d3f9b80e6cc161b09819
swethapandiri878 <swethapandiri987@gmail.com>
2026-02-22 18:29:37 +0530

    alu_pipelined.v

--- rtl/alu_pipelined.v
+++ rtl/alu_pipelined.v
@@ -1,4 +1,46 @@
 // Pipelined ALU Verilog module
-module alu_pipelined(input wire [31:0] a, b, input wire [2:0] opcode, output reg [31:0] result);
-    // ALU implementation goes here
+module alu_pipelined #(parameter width = 4)(
+    input clk,
+    input reset,
+    input [width-1:0] A,
+    input [width-1:0] B,
+    input [3:0] opcode,
+    output reg [2*width-1:0] result
+);
+
+reg [width-1:0] A_reg, B_reg;
+reg [3:0] opcode_reg;
+reg [2*width-1:0] exe_result;
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        A_reg <= 0;
+        B_reg <= 0;
+        opcode_reg <= 0;
+    end
+    else begin
+        A_reg <= A;
+        B_reg <= B;
+        opcode_reg <= opcode;
+    end
+end
+
+always @(*) begin
+    case(opcode_reg)
+        4'b0000: exe_result = A_reg + B_reg;   // ADD
+        4'b0001: exe_result = A_reg - B_reg;   // SUB
+        4'b0010: exe_result = A_reg & B_reg;   // AND
+        4'b0011: exe_result = A_reg | B_reg;   // OR
+        4'b0100: exe_result = A_reg * B_reg;   // MUL
+        default: exe_result = 0;
+    endcase
+end
+
+always @(posedge clk or posedge reset) begin
+    if (reset)
+        result <= 0;
+    else
+        result <= exe_result;
+end
+
 endmodule
